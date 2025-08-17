9bbd2b7e26814eac504e4802bfdeb9159b159147
Consita <51132958+Consita@users.noreply.github.com>
2025-08-17 11:17:26 +0200

    Add files via upload

---
+++ TBC-Launch-Solo-Turnin.lua
@@ -0,0 +1,14 @@
+RXPGuides.RegisterGuide([[
+#classic
+#version 3
+#group Consita Classic/TBC Launch Guide
+#subgroup 3 - TBC Launch Day
+#name Solo Turn-in
+
+-- |cRXP_FRIENDLY_, |cRXP_ENEMY_, |cRXP_LOOT_, |cRXP_WARN_, |cRXP_PICK_, |cRXP_BUY_
+-- |c99ffff99 OR|r
+-- |Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk
+
+step
+    >>Welcome to 
+]])
