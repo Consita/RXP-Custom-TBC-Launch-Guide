d2449610f6eb009a57e13e1012af69fe511ba837
Consita <jan.rechel@googlemail.com>
2025-11-11 14:27:36 +0100

    final fix for mc and ubrs

--- TBC/Dungeon-Quest-Preparation-Strat.lua
+++ TBC/Dungeon-Quest-Preparation-Strat.lua
@@ -620,6 +620,19 @@ RXPGuides.RegisterGuide([[
 		.mob Halycon
 		.complete 4724,1
 		.isOnQuest 4724
+
+	step
+		#completewith next
+		.zone Azshara >>Travel to |cFFfa9602Azshara|r
+		.zoneskip Azshara
+
+	step
+		.goto Azshara,79.2,73.6,5,0
+		>>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Duke Hydraxis|r
+		.turnin 6821
+		.accept 6822
+		.target Duke Hydraxis
+
 ]])
 
 RXPGuides.RegisterGuide([[
