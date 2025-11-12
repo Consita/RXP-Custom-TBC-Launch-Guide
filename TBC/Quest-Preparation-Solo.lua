78ef5a085a493d0c5020418afb1372574a110ce4
Consita <jan.rechel@googlemail.com>
2025-11-12 12:23:58 +0100

    removed deseurter and removed twilight lexicno pickup/turnin

--- TBC/Quest-Preparation-Solo.lua
+++ TBC/Quest-Preparation-Solo.lua
@@ -1012,24 +1012,10 @@ RXPGuides.RegisterGuide([[
 			.accept 8320
 
 		step
-			.goto Silithus,49.7,37.5,5,0
-			>>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Geologist Larksbane|r
-			.target Geologist Larksbane
-			.accept 8285
-
-		step
 			.goto Silithus,49.20,34.30
 			>>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Mar'alith|r
 			.accept 8304
 			.target Commander Mar'alith	
-
-		step
-			.goto Silithus,67,69.6
-			>>Find and speak to |cRXP_FRIENDLY_Hermit Ortell|r (The Deserter)
-			.complete 8285,1
-			.target Hermit Ortell
-			.turnin 8285
-			.accept 8279
 
 		step
 			.goto Silithus,41.30,88.50,10,0
@@ -1060,7 +1046,7 @@ RXPGuides.RegisterGuide([[
 			>>Kill |cRXP_ENEMY_Twilight Keeper Havunth|r. Loot him for his |cRXP_LOOT_Twilight Lexicon Chapter|r
 			>>He patrols the east side of the camp that's just southwest of Cenarion Hold
 			>>|cRXP_WARN_Be careful, he has very high burst|r
-			.complete 8279,3 
+			.complete 20396 
 			.unitscan Twilight Keeper Havunth
 			.mob Twilight Keeper Havunth
 
@@ -1069,7 +1055,7 @@ RXPGuides.RegisterGuide([[
 			>>Kill |cRXP_ENEMY_Twilight Keeper Mayna|r. Loot her for her |cRXP_LOOT_Twilight Lexicon Chapter|r
 			>>She patrols the camp far west of Cenarion Hold
 			>>|cRXP_WARN_Be careful, she has Psychic Scream (instant fear)|r
-			.complete 8279,1 
+			.collect 20394 
 			.unitscan Twilight Keeper Mayna
 			.mob Twilight Keeper Mayna
 
@@ -1078,7 +1064,7 @@ RXPGuides.RegisterGuide([[
 			>>Kill |cRXP_ENEMY_Twilight Keeper Exeter|r. Loot him for his |cRXP_LOOT_Twilight Lexicon Chapter|r
 			>>He's at the back of the camp very far to the southwest of Cenarion Hold/northwest of Ahn'Qiraj
 			>>|cRXP_WARN_Be careful, he has a stun and mortal strike|r
-			.complete 8279,2 
+			.complete 20395 
 			.unitscan Twilight Keeper Exeter
 			.mob Twilight Keeper Exeter
 
@@ -1248,13 +1234,6 @@ RXPGuides.RegisterGuide([[
 			.turnin 1126 >>Turn in Hive in the Tower
 			.accept 6844 >>Accept Umber, Archivist
 			.target Layo Starstrike	
-
-		step
-			.goto Silithus,67,69.6
-			>>|cRXP_WARN_This quests can be used as optional turn in quests for TBC. Check /tbcprep to see if you need it|r
-			>>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Hermit Ortell|r
-			.target Hermit Ortell
-			.turnin 8279
 
 		step
 			#completewith silithusglyph
